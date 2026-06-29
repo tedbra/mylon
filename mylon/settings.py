381b53e56eb63d05fab07a0539279077785ae6a9
tedbra <teddadios@gmail.com>
2026-06-29 04:58:01 +0200

    Shifting the Secret key to be secrete

--- mylon/settings.py
+++ mylon/settings.py
@@ -12,9 +12,12 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+load_dotenv(os.path.join(BASE_DIR, '.env'))
 
 
 # Quick-start development settings - unsuitable for production
